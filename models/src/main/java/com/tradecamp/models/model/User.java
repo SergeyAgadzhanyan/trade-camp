6af308588f3af4662faa5a81a089b7ef78498b5a
Sergey Agadzhanyan <sergey.agadzhanyan12@gmail.com>
2024-06-16 15:51:17 +0300

    Добавил миграцию базы данных. Теперь name в таблице user имеет unique constraint

--- models/src/main/java/com/tradecamp/models/model/User.java
+++ models/src/main/java/com/tradecamp/models/model/User.java
@@ -14,7 +14,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String name;
     private String password;
 }
